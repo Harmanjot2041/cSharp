947d8eb8b558b138731537f57389ce06aa03a716
Harmanjot2041 <71279336+Harmanjot2041@users.noreply.github.com>
2021-02-16 09:09:24 +0530

    collections ,arrays types
    arrayList , hash tables ,array types etc..

--- cSharp_practice/cSharp_practice/Program.cs
+++ cSharp_practice/cSharp_practice/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,27 +10,29 @@ namespace cSharp_practice
     class Program
     {
         enum Days { Sun, Mon, tue, Wed, thu, Fri, Sat };
-        
-        static void  Main(string[] args)
+
+        static void Main(string[] args)
         {
             //Console.WriteLine(args);
             Area rectangle = new Area();
+            Console.Write("Enter length of Reactangle: ");
             double length = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter width of Reactangle: ");
             double width = Convert.ToDouble(Console.ReadLine());
-            rectangle.AcceptDetails(length,width);
+            rectangle.AcceptDetails<double>(12.4, 25.3);
             Console.WriteLine("Area of Rectangle is: " + rectangle.GetArea());
             int temp = 37;
             Console.WriteLine(temp.ToString());
-            for(int i = 0;i <5;i++)
+            for (int i = 0; i < 5; i++)
             {
                 if (i == 3)
                 {
                     continue;
                 }
                 Console.WriteLine(i);
-               
+
             }
-            
+
             int Factorial(int num)
             {
                 int factorial = 1;
@@ -39,10 +42,10 @@ namespace cSharp_practice
                 }
                 return factorial;
             }
-
+            Console.Write("Enter number for factorial: ");
             int number = Convert.ToInt32(Console.ReadLine());
             int result = Factorial(number);
-            Console.WriteLine("factorial of " + number + " is :" +  result);
+            Console.WriteLine("factorial of " + number + " is :" + result);
 
             string[] data =
             {
@@ -53,7 +56,7 @@ namespace cSharp_practice
                 "harmnn4"
             };
             Console.WriteLine("Foreach loop on string array :");
-            foreach(var name in data)
+            foreach (var name in data)
             {
                 Console.WriteLine(name);
             }
@@ -63,15 +66,15 @@ namespace cSharp_practice
             {
                 var num = 5;
                 var num2 = 0;
-                a = num/ num2;
+                a = num / num2;
             }
-            catch(ArithmeticException e)
+            catch (ArithmeticException e)
             {
-                Console.WriteLine("hii infinite you are handled" );
-            } 
+                Console.WriteLine("hii infinite you are handled");
+            }
             finally
             {
-                Console.WriteLine("hello finally "+ a);
+                Console.WriteLine("hello finally " + a);
             }
             Console.WriteLine(Days.Mon);
             dynamic value1 = "myType";
@@ -86,18 +89,80 @@ namespace cSharp_practice
                 Console.WriteLine(*ptr);
             }
             int[] arr = new int[] { 1, 2, 4, 5 };
-            Console.WriteLine("1D  dynamic array: " +arr[3]);
+            Console.WriteLine("1D  dynamic array: " + arr[3]);
             Console.WriteLine("array elements after loop:");
-            for(int i=0 ; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 ++arr[i];
                 Console.WriteLine(arr[i]);
             }
+            int[,] matrix = new int[,] {
+                {1,2,3 },
+                {2,3,4 },
+                { 3, 4, 5},
+                {4,5,6 }
+            };
+            Console.WriteLine("Matrix (multidem array) :");
+            for(int i=0;i< 4;i++)
+            {
+                for(int j = 0;j<3;j++)
+                {
+                    Console.Write(matrix[i, j]+ "  ");
+                }
+                Console.WriteLine(" ");
+                
+            }
+            int[][] obj = new int[4][];
+            for(int i=0;i<4;i++)
+            {
+                obj[i] = new int[3];
+            }
+            // jagged array
+            Console.WriteLine("Jagged array: ");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(obj[i][j] + "  ");
+                }
+                Console.WriteLine(" ");
+
+            }
+            //ArrayList collections 
+            ArrayList arrList = new ArrayList();
+            Console.WriteLine("Size of ArrayList when NO element is inserted: " + arrList.Capacity);
+            arrList.Add(1);
+            Console.WriteLine("Size of ArrayList when 1 element is inserted: " + arrList.Capacity);
+            arrList.Add(2);
+            arrList.Add(3);
+            arrList.Add(4);
+            arrList.Add(5);
+            Console.WriteLine("Size of ArrayList when 4 element is inserted: " + arrList.Capacity);
+            arrList.Remove(3);
+            foreach (object item in arrList)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+            //Hashed Tables collections
+            Hashtable hashTable = new Hashtable();
+            hashTable.Add("Name", "Harman");
+            hashTable.Add("Age", 21);
+            hashTable.Add("id", "happy16harman@gmail.com");
+            hashTable.Add("collage", "UIET PANJAB UNIVERSITY CHD");
+            Console.WriteLine("Data from hashTables : ");
+            foreach(object key in hashTable.Keys)
+            {
+                Console.WriteLine(key + " : " + hashTable[key]);
+            }
+            //Console.WriteLine("Name from Hashtable Data : " + hashTable["Name"]);
+            //Console.WriteLine("Age from Hashtable Data : " + hashTable["Age"]);
+            //Console.WriteLine("id from Hashtable Data : " + hashTable["id"]);
+            //Console.WriteLine("Collage from Hashtable Data : " + hashTable["collage"]);
+
             string myName = "harman";
             Console.WriteLine("string: " + myName);
-            //int* ptr = &val2;
-            //Console.WriteLine((int)&val2);
-            //Console.WriteLine(*ptr);
+            
             Console.WriteLine("value with ++ oprator: " + val3++);
             val1 = val2 & val3;
             Console.WriteLine(" value with bitwise AND "+val1);
@@ -120,10 +185,11 @@ namespace cSharp_practice
     {
         double length;
         double width;
-        public void AcceptDetails(double length, double width)
+        public void AcceptDetails<T>(T length,T width)
         {
-            this.length = length;
+             this.length = length;
             this.width = width;
+            //Console.WriteLine(length);
         }
         public double GetArea()
         {
